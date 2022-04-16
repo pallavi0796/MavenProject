d775851310991ea4423a1c14a0a11fb5fa40e038
ADMIN <ADMIN@192.168.1.38>
2022-04-16 08:30:06 +0530

    new project

---
+++ MavenProject1/src/main/java/com/stepdef/LoginStepdef.java
@@ -0,0 +1,41 @@
+package com.stepdef;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginStepdef {
+	WebDriver driver;
+	
+	@Given("^user should be on login page$")
+	public void user_should_be_on_login_page() throws Throwable {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+	    driver.get("file:///D:/SELENIUM/javabykiran-Selenium-Softwares/Offline%20Website/index.html");
+	   
+	}
+
+	@When("^user enters valid credentials$")
+	public void user_enters_valid_credentials() throws Throwable {
+		driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("123456");
+		driver.findElement(By.xpath("//button")).click();
+		/*driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("kiran@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("123456");
+		driver.findElement(By.xpath("//*[@id=\"form\"]/div[3]/div/button")).click();*/
+	}
+
+	@Then("^user should be on home page$")
+	public void user_should_be_on_home_page() throws Throwable {
+		 Assert.assertEquals("JavaByKiran | Dashboard", driver.getTitle());
+	}
+
+}
